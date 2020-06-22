d6f28e90627a6dfd5f090adb5628358028393019
Sizon <sizon@vidrada.org>
2020-06-22 07:56:43 +0300

    fix bugs

--- script/main.js
+++ script/main.js
@@ -1,6 +1,19 @@
 const mobileMenu = document.querySelector('.mobile-menu'),
   menuSidebar = document.querySelector('.menu-sidebar');
 
+// const footerButtons = document.querySelector('.footer-buttons'),
+//   buttonItems = document.querySelector('.button-item'),
+//   counterTitle = document.querySelector('.counter-title'),
+//   counterText = document.querySelector('.counter-text');
+
+// Mobile menu
 mobileMenu.addEventListener('click', () => {
   menuSidebar.classList.toggle('menu-sidebar__active');
-})
+});
+
+// Yore page footer counter
+// footerButtons.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   console.log("click");
+//   console.log('event: ', event);
+// })
